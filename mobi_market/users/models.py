9e44ebab6dd2aed3906ab1988ff2e55bed05f880
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-06-13 11:01:56 +0300

    Added user model

--- mobi_market/users/models.py
+++ mobi_market/users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    avatar = models.CharField(max_length=250)
+    birth_date = models.DateField(blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.pk} - {self.email}"
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
