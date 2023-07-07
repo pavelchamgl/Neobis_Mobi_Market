087cd2565d2377c15e21ceaee26101bf2f3ec948
Pavel <pavel@MacBook-Pro-Pavel.local>
2023-07-07 18:47:56 +0300

    Fix urls in products app

--- mobi_market/products/urls.py
+++ mobi_market/products/urls.py
@@ -9,7 +9,7 @@ router.register('', UserCardItemViewSet, basename='my_products')
 urlpatterns = [
     path('api/products/my/', include(router.urls)),
     path('api/products/all/', CardItemListAPIView.as_view()),
-    re_path('api/product/(?P<id>.+)/', CardItemRetrieveAPIView.as_view()),
+    re_path('api/products/(?P<id>.+)/', CardItemRetrieveAPIView.as_view()),
     re_path('api/like/(?P<product_id>.+)/product/', CardItemLikeAPIView.as_view(), name='card_item_like'),
     re_path('api/unlike/(?P<product_id>.+)/product/', CardItemUnlikeAPIView.as_view(), name='card_item_unlike'),
     path('api/favourite_products/list/', FavouriteCardItemListApiView.as_view()),
